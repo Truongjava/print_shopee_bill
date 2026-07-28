965992e37b55dbb5495e999629288fdf7066c436
Truongjava <thanhtruong16042004@gmail.com>
2026-07-28 14:06:44 +0700

    Clean up temp files, add PyInstaller spec

--- main_app_recovered.py
+++
@@ -1,6 +0,0 @@
-# decompyle3 version 3.9.3
-# Python bytecode version base 3.11 (3495)
-# Decompiled from: Python 3.11.8 (tags/v3.11.8:db85d51, Feb  6 2024, 22:03:32) [MSC v.1937 64 bit (AMD64)]
-# Embedded file name: main_app.py
-# Compiled at: 2026-07-25 11:55:47
-# Size of source mod 2**32: 145376 bytes
